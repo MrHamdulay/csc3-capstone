dc7dde238919abc694382434c890e776d482801b
Yaseen Hamdulay <yaseen@hamdulay.co.za>
2014-08-15 13:35:26 +0200

    comment winnower algorithm

--- cheaters/algorithms/winnoweralgorithm.py
+++ cheaters/algorithms/winnoweralgorithm.py
@@ -4,6 +4,9 @@ from collections import deque, defaultdict
 WINDOW_LENGTH = 13
 NGRAM_LENGTH = 5
 
+'''
+given a string and a length generate and return all
+length-grams of this string'''
 def ngrams(string, ngram_length):
   line_number = 0
   for i in xrange(len(string)-ngram_length):
@@ -12,10 +15,19 @@ def ngrams(string, ngram_length):
     ngram = linestring[i:i+ngram_length]
     yield (line_number, ngram)
 
+
+'''
+generate all ngrams and then hash the string
+'''
 def ngrams_hashes(string, ngram_length):
   for line_number, ngram in ngrams(string, ngram_length):
     yield line_number, hash(ngram)
 
+
+'''
+go through every window of length n and choose the minimum
+n-gram hash within that window
+'''
 def window_hashes(string):
   ngram_iterator = ngrams(string, NGRAM_LENGTH)
   ngram_window = deque()
@@ -28,6 +40,11 @@ def window_hashes(string):
     current_ngrams_hashes.popleft()
     ngram_window.append(ngram_iterator.next())
 
+
+'''
+algorithm that implements the winnower search method described
+in the MOSS whitepaper
+'''
 class WinnowerAlgorithm(BaseAlgorithm):
   ALLOWED_LANGUAGE_EXTENSIONS = ['*']
 
@@ -37,9 +54,13 @@ class WinnowerAlgorithm(BaseAlgorithm):
   def isPlagiarised(self, program):
     canonicalised_ = program.canonicalised_program_source
 
+    # generate and store all winnower hashes
     program_matches = defaultdict(list)
     for line_number, h in window_hashes(program.canonicalised_program_source):
       if h in self.ngrams:
         for program, match_line_number in self.ngrams[h]:
           program_matches[program].append((line_number, match_line_number))
       self.ngrams[h].append((program, line_number))
+
+    # TODO: look up winnower hashes from other programs and calculate
+    # a "matching rating"
