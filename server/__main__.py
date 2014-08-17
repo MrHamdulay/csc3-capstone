8093161e4b8070b7e35a08feccb365d3cf5feffb
Jarred de Beer <jarr2000@gmail.com>
2014-08-17 16:35:26 +0200

    adding routes for assignments and submission listings

--- server/__main__.py
+++ server/__main__.py
@@ -5,26 +5,51 @@ app = Flask(__name__)
 
 class View(FlaskView):
 
-  '''
-  Web form to submit assignments. This is the starting point.
-  @GET /
-  @render submit.html
-  '''
-  def index(self):
-    return render_template('submit.html')
+    '''
+    Web form to submit assignments. This is the starting point.
+    @GET /
+    @render submit.html
+    '''
+    @route('/')
+    def index(self):
+        return render_template('submit.html')
 
-  '''
-  Receive and Process assignments submitted from the web form
-  @POST /submit
-  @render :)
-  '''
-  @route('/submit', methods=['POST'])
-  def upload_file(self):
-    submission = request.files['submission']
-    assignment_number = request.form['assignment_number']
-    Detector.run(submission, assignment_number)
-    return ':)'
+    '''
+    Receive and Process assignments submitted from the web form
+    @POST /submit
+    @render :)
+    '''
+    @route('/submit', methods=['POST'])
+    def upload_file(self):
+        submission = request.files['submission']
+        assignment_number = request.form['assignment_number']
+        Detector.run(submission, assignment_number)
+        return ':)'
+
+    '''
+    Display results:
+        -Assignment
+            -List of submissions, e.g. DBRJAR001 (90%)
+                - 2 way diffs
+    '''
+    @route('/assignments')
+    def list_of_assignments(self):
+        # TODO: get assignments from db
+        # TODO: return template
+        return ':]'
+
+    @route('/assignments/<assignment_id>')
+    def assignment(self, assignment_id, submission_id):
+        # TODO: get results from db
+        # TODO: return template
+        return ':D'
+
+    @route('/assignments/<assignment_id>/<submission_id>')
+    def list_of_submissions(self, id):
+        # TODO: get results from db
+        # TODO: return template
+        return 'XD'
 
 if __name__ == '__main__':
-  View.register(app)
-  app.run(debug=True)
+    View.register(app)
+    app.run(debug=True)
