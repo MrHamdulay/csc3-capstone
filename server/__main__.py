14d43b2d635fb1c6419ddd93f0e87a83dfe1cdcf
Jarred de Beer <jarr2000@gmail.com>
2014-08-17 13:48:55 +0200

    removing zipfile from main

--- server/__main__.py
+++ server/__main__.py
@@ -1,9 +1,7 @@
-import zipfile
 from cheaters.detector import Detector
 from flask import Flask, render_template, request
 from flask.ext.classy import FlaskView, route
 app = Flask(__name__)
-
 
 class View(FlaskView):
 
@@ -24,34 +22,8 @@ class View(FlaskView):
   def upload_file(self):
     submission = request.files['submission']
     assignment_number = request.form['assignment_number']
-    Detector.run(assignment_number, submission)
+    Detector.run(submission, assignment_number)
     return ':)'
-
-  '''
-  parses the submitted zip archive file
-  @param String archive, provided by form POST body
-  '''
-  def parse_archive(self, archive):
-    zf = zipfile.ZipFile(archive)
-    files = []
-    for info in zf.infolist():
-      files.append(self.get_filename(info.filename))
-    return files
-
-  '''
-  Extracts the file name from the path listed out the zip archive
-  'stntnm001/**/filename.py' -> 'filename.py'
-  @param String path
-  @return String
-  '''
-  def get_filename(self, path):
-    return path.split('/')[-1]
-
-  '''
-  Extracts the student number from the submitted file name
-  '''
-  def get_student_number(self, path):
-    return path.split('.')[0]
 
 if __name__ == '__main__':
   View.register(app)
