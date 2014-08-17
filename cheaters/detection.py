54cf0ffe657cd339dd75606c8b328e20feda7336
Yaseen Hamdulay <yaseen@hamdulay.co.za>
2014-08-17 13:47:44 +0200

    initial detector encapsulation

--- cheaters/detection.py
+++ cheaters/detection.py
@@ -1,6 +1,28 @@
+import re
+import zipfile
+
+
+languages = ['py', 'java']
 class Detector:
-  ''' Encapsulates the entire parsing, signature generation and detection process '''
-  def submit(self, submission):
-    ''' '''
-    program
-    algorithm
+    ''' Encapsulates the entire parsing, signature generation and detection process '''
+
+    def submit(self, files, assignment):
+        '''
+        @param files ZipFile object that contains students files
+        @param assignment id of assignment submitting to
+        '''
+        assert isinstance(files,
+        language = self.detect_language(files)
+        submission = self.concatenate_files(files)
+        algorithm
+
+    def strip_unnecessary_files(self, files):
+        '''
+        @param submission list of files'''
+        pass
+
+
+    def detect_language(self, files):
+        '''
+        @param submission list of files'''
+
---
+++ cheaters/languages/__init__.py
@@ -0,0 +1,4 @@
+file_map = {
+#        'py': PythonParser,
+#        'java': JavaParser
+}
