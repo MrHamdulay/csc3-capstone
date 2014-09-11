942e4d0fa048271fecca16dcc1860af1357279fb
merishlalla <merish-lalla@hotmail.co.nz>
2014-09-11 11:50:24 +0200

    finished java thingy

--- cheaters/languages/java/parser.py
+++ cheaters/languages/java/parser.py
@@ -1,7 +1,23 @@
 from programsubmission import ProgramSubmission
-from java import lexer
+import plyj.parser as plyj
 
 
+parser = plyj.Parser()
 class JavaLanguageHandler(ProgramSubmission):
     file_types = ['java']
-    pass
+
+    def strip_unstable_atrributes(self, submission):
+        ''' Function that imports plyj which has tokens and keywords set up and then manipulated in order to parse the correct
+        values to the AST
+        '''
+        parser.lexer.input(submission)
+        value = ""
+        line = 1
+        for token in parser.lexer:
+            while token.lineno >= line:
+                value = value + "\n"
+                line += 1
+            value += token.type[:3]
+
+
+        return value
