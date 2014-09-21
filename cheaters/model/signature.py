a444968dab231a778df344cc660b2395853ebdcb
merishlalla <merish-lalla@hotmail.co.nz>
2014-09-21 17:25:37 +0200

    Tests to be fixed

--- cheaters/model/signature.py
+++ cheaters/model/signature.py
@@ -15,6 +15,7 @@ class Signature:
         self.line_number_mine = line_number
         self.submission_id_theirs = submission_id_theirs
         self.line_number_theirs = line_number_theirs
+        print(submission_id_mine, submission_id_theirs)
         assert submission_id_mine != submission_id_theirs or submission_id_mine is None
 
     def reverse(self):
@@ -22,4 +23,4 @@ class Signature:
         return Signature(self.ngram_hash, self.submission_id_theirs, self.line_number_theirs, self.submission_id_mine, self.line_number_mine)
 
     def __repr__(self):
-        return '<Signature hash:"%s" id:%s line_number:%s their_line_number:%s>' % (self.ngram_hash, self.submission_id_mine, self.line_number_mine, self.line_number_theirs)
+        return '<Signature hash:"%s" id:%s line_number:%s their_line_number:%s %s %s>' % (self.ngram_hash, self.submission_id_mine, self.line_number_mine, self.line_number_theirs, self.submission_id_mine, self.submission_id_theirs)
